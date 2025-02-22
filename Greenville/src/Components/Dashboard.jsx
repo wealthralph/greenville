f3af161007c5f9e4a044025b5061c807d3851410
Kayode_Oguntala <oguntalakayode@gmail.com>
2025-02-22 16:15:26 +0100

    formatted dashboard.jsx

--- Greenville/src/Components/Dashboard.jsx
+++ Greenville/src/Components/Dashboard.jsx
@@ -2,21 +2,20 @@ import Greetings from "./Greetings";
 import DashboardCard from "./DashboardCard";
 import Barchart from "./Barchart";
 import PickupStats from "./PickupStats";
-import PaymentMeter from './PaymentMeter';
+import PaymentMeter from "./PaymentMeter";
 import HamburgerMenu from "./HamburgerMenu";
 import History from "./History";
 // import FloatingButton from "./FloatingRoundButton";
 export default function Dashboard() {
   return (
     <>
-    {/* <HamburgerMenu /> */}
+      {/* <HamburgerMenu /> */}
       <Greetings name={"Elo"} />
       <DashboardCard />
       <PaymentMeter />
       <PickupStats />
       <Barchart />
       <History />
-   
     </>
   );
 }
