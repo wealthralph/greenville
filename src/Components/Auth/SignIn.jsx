2e0e8147cb2fa90b9f08704d24ec22ff792a86c9
SHITT QUDUS.A <shittuqadekunle@gmail.com>
2025-03-31 09:08:55 -0700

    componets updated

---
+++ src/Components/Auth/SignIn.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "../..style/signin.css"
+
+export default function SignIn() {
+  const [email,setEmail]= useState('')
+  const [password, setPassword] = useState('')
+
+  const handleEmail = (e)=>{
+    setEmail(e.target.value);
+  }
+  const handlePassword = (e)=>{
+    setPassword(e.target.value);
+  }
+  return (
+
+    <div className="signin_con"> 
+    
+    <div className="greencard">
+    <h2 className="greentext">Welcome to <span>Greenville</span></h2> 
+    <h3 className="greensubtext">Join Us in Keeping the Planet Clean – Sign Up & Make a Difference! 🌍♻️</h3>
+        <br></br> <br></br> 
+    <div className="imagediv">
+  <img src="..\src\assets\images\Greenv.png" id="image"  alt="GREENVILLE"/>
+    </div>
+    </div> 
+    <br></br>
+  
+    
+    <div className="logincard">
+    
+      <form action="submit" className="signin_form"><h1 className="signin_header" style={{color:" #1B5B33"}}><b>sign in</b></h1>
+
+        <label className="label">
+          Email
+          <input type="email" name="Email" placeholder="Enter Email" onChange={handleEmail} value={email} required />
+        </label>
+        <label className="label">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            onChange={handlePassword}
+            value={password}
+            required
+          />
+        </label>
+        <div className="signin_btns">
+          <button className="login-btn" style={{color:"white"}}>Login</button> 
+        <button className="signin_btn">Sign In with Google</button>
+        <Link to="/Forgot Password"  className="fp">Forgot Password?</Link>
+        </div>
+        <p className="nh">
+        New to Greenville? <Link to="/Sign Up">Sign Up</Link>
+      </p>
+      </form>
+     
+    </div>
+    </div>
+  );
+}
---
+++ src/Components/Home components/AreasWeCover.jsx
@@ -0,0 +1,36 @@
+
+
+export default function Areas(){
+    return(
+        <>
+         <div className="areaContainer">
+        <h1 className="areaHeader">Areas we cover</h1>
+            <div className="firstRow">
+            <div className="ikeja">
+                <h3>Ikeja</h3>
+            </div>
+            <div className="vi">
+                <h3>V.I</h3>
+            </div>
+            </div>
+            <div className="secondRow">
+            <div className="jakande">
+                <h3>Jakande</h3>
+            </div>
+            <div className="ikate">
+                <h3>Ikate</h3>
+            </div>
+            </div>
+            <div className="thirdRow">
+            <div className="chevron">
+                <h3>Chevron</h3>
+            </div>
+            <div className="ikoyi">
+                <h3>Ikoyi</h3>
+            </div>
+            </div>
+           
+        </div>
+        </>
+    )
+}
