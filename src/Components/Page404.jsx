2a9f111f911b273c0fbce27af9b2f999f419cd1b
Elochukwu Ezeaneche <henryezeaneche@gmail.com>
2025-04-02 16:50:54 +0100

    Delete src directory

--- src/Components/Page404.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom"
-export default function Page404(){
-    return(
-        <div>
-            <h1>Page Not Found</h1>
-            <Link to="/">Homepage</Link>.
-        </div>
-    )
-}
