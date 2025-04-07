efba04a28cdc5b9512c1243d4b9aa20bd918e448
SHITT QUDUS.A <shittuqadekunle@gmail.com>
2025-04-07 09:02:10 -0700

    latest update

--- Greenville/src/Components/Auth/SignIn.jsx
+++ Greenville/src/Components/Auth/SignIn.jsx
@@ -12,10 +12,24 @@ export default function SignIn() {
     setPassword(e.target.value);
   }
   return (
-    <div className="signin_con">
+
+    <div className="signin_con"> 
     
-    <h1 className="signin_header">Sign In</h1>
-      <form action="submit" className="signin_form">
+    <div className="greencard">
+    <h2 className="greentext">Welcome to <span>Greenville</span></h2> 
+    <h3 className="greensubtext">Join Us in Keeping the Planet Clean – Sign Up & Make a Difference! 🌍♻️</h3>
+        <br></br> <br></br> 
+    <div className="imagediv">
+  <img src="..\src\assets\images\signup.png" id="image"  alt="GREENVILLE"/>
+    </div>
+    </div> 
+    <br></br>
+  
+    
+    <div className="logincard">
+    
+      <form action="submit" className="signin_form"><h1 className="signin_header" style={{color:" #1B5B33"}}><b>sign in</b></h1>
+
         <label className="label">
           Email
           <input type="email" name="Email" placeholder="Enter Email" onChange={handleEmail} value={email} required />
@@ -32,83 +46,16 @@ export default function SignIn() {
           />
         </label>
         <div className="signin_btns">
-        <button className="signin_btn">Sign In</button>
+          <button className="login-btn" style={{color:"white"}}>Login</button> 
+        <button className="signin_btn">Sign In with Google</button>
         <Link to="/Forgot Password"  className="fp">Forgot Password?</Link>
         </div>
-       
-      </form>
-      <p className="nh">
-        New here? <Link to="/Sign Up">Sign Up</Link>
+        <p className="nh">
+        New to Greenville? <Link to="/Sign Up">Sign Up</Link>
       </p>
+      </form>
+     
+    </div>
     </div>
   );
 }
-
-
-
-
-
-
-
-// import { useState } from "react";
-// import { Link } from "react-router-dom";
-// import "../..style/signin.css"
-
-// export default function SignIn() {
-//   const [email,setEmail]= useState('')
-//   const [password, setPassword] = useState('')
-
-//   const handleEmail = (e)=>{
-//     setEmail(e.target.value);
-//   }
-//   const handlePassword = (e)=>{
-//     setPassword(e.target.value);
-//   }
-//   return (
-
-//     <div className="signin_con"> 
-    
-//     <div className="greencard">
-//     <h2 className="greentext">Welcome to <span>Greenville</span></h2> 
-//     <h3 className="greensubtext">Join Us in Keeping the Planet Clean – Sign Up & Make a Difference! 🌍♻️</h3>
-//         <br></br> <br></br> 
-//     <div className="imagediv">
-//   <img src="..\src\assets\images\Greenv.png" id="image"  alt="GREENVILLE"/>
-//     </div>
-//     </div> 
-//     <br></br>
-  
-    
-//     <div className="logincard">
-    
-//       <form action="submit" className="signin_form"><h1 className="signin_header" style={{color:" #1B5B33"}}><b>sign in</b></h1>
-
-//         <label className="label">
-//           Email
-//           <input type="email" name="Email" placeholder="Enter Email" onChange={handleEmail} value={email} required />
-//         </label>
-//         <label className="label">
-//           Password
-//           <input
-//             type="password"
-//             name="password"
-//             placeholder="Enter Password"
-//             onChange={handlePassword}
-//             value={password}
-//             required
-//           />
-//         </label>
-//         <div className="signin_btns">
-//           <button className="login-btn" style={{color:"white"}}>Login</button> 
-//         <button className="signin_btn">Sign In with Google</button>
-//         <Link to="/Forgot Password"  className="fp">Forgot Password?</Link>
-//         </div>
-//         <p className="nh">
-//         New to Greenville? <Link to="/Sign Up">Sign Up</Link>
-//       </p>
-//       </form>
-     
-//     </div>
-//     </div>
-//   );
-// }
--- Greenville/src/Components/Home components/Footer.jsx
+++ Greenville/src/Components/Home components/Footer.jsx
@@ -1,32 +1,17 @@
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer_links">
+      
         <section className="footer_section">
-          <h3 className="footer_heading">Useful Links</h3>
+          
           <ul className="footer_list">
-            <li>
-              <a href="#" className="footer_link">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="footer_link">
-                Partners
-              </a>
-            </li>
-            <li>
-              <a href="#" className="footer_link">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </section>
-
-        <section className="footer_section">
           <h3 className="footer_heading">Help?</h3>
-          <ul className="footer_list">
+           
             <li>
+            
               <a href="#" className="footer_link">
                 FAQ
               </a>
@@ -61,108 +46,40 @@ export default function Footer() {
           </p>
         </section>
 
-        <section className="footer_social-media">
-          <a href="#" className="footer_social-link" aria-label="Facebook">
-            <i className="socialmedia-icon facebook"></i>
-          </a>
-          <a href="#" className="footer_social-link" aria-label="Twitter">
-            <i className="socialmedia-icon twitter"></i>
-          </a>
-          <a href="#" className="footer_social-link" aria-label="Instagram">
-            <i className="socialmedia-icon instagram"></i>
-          </a>
-          <a href="#" className="footer_social-link" aria-label="Send Message">
-            <i className="socialmedia-icon paper-plane"></i>
-          </a>
+        <section className="footer_section">
+
+          <h3 className="footer_heading">Useful Links</h3>
+          <ul className="footer_list">
+            <li>
+              <a href="#" className="footer_link">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer_link">
+                Partners
+              </a>
+            </li>
+            <li>
+              <a href="#" className="footer_link">
+                Contact
+              </a>
+            </li>
+          </ul>
         </section>
+        <section className="footer_social-media">
+              <a href="#" className="footer_social-link" aria-label="Facebook">
+                <FaFacebookF className="socialmedia-icon facebook" />
+              </a>
+              <a href="#" className="footer_social-link" aria-label="Twitter">
+                <FaTwitter className="socialmedia-icon twitter" />
+              </a>
+              <a href="#" className="footer_social-link" aria-label="Instagram">
+                <FaInstagram className="socialmedia-icon instagram" />
+              </a>
+              <h3>© Copyright 2025</h3>
+            </section>
       </div>
     </footer>
   );
 }
-
-
-// import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
-
-// export default function Footer() {
-//   return (
-//     <footer className="footer">
-//       <div className="footer_links">
-      
-//         <section className="footer_section">
-          
-//           <ul className="footer_list">
-//           <h3 className="footer_heading">Help?</h3>
-           
-//             <li>
-            
-//               <a href="#" className="footer_link">
-//                 FAQ
-//               </a>
-//             </li>
-//             <li>
-//               <a href="#" className="footer_link">
-//                 Terms & Conditions
-//               </a>
-//             </li>
-//             <li>
-//               <a href="#" className="footer_link">
-//                 Privacy
-//               </a>
-//             </li>
-//             <li>
-//               <a href="#" className="footer_link">
-//                 Policy
-//               </a>
-//             </li>
-//           </ul>
-//         </section>
-
-//         <section className="footer_section">
-//           <h3 className="footer_heading">Contact</h3>
-//           <p className="footer_paragraph">
-//             No. 3 Segun Festus Oluwa street, off Anthony Enahoro, Ikoyi, Lagos
-//           </p>
-//           <p className="footer_paragraph">
-//             <a href="mailto:contact@greenville.com" className="footer_link">
-//               contact@greenville.com
-//             </a>
-//           </p>
-//         </section>
-
-//         <section className="footer_section">
-
-//           <h3 className="footer_heading">Useful Links</h3>
-//           <ul className="footer_list">
-//             <li>
-//               <a href="#" className="footer_link">
-//                 About
-//               </a>
-//             </li>
-//             <li>
-//               <a href="#" className="footer_link">
-//                 Partners
-//               </a>
-//             </li>
-//             <li>
-//               <a href="#" className="footer_link">
-//                 Contact
-//               </a>
-//             </li>
-//           </ul>
-//         </section>
-//         <section className="footer_social-media">
-//               <a href="#" className="footer_social-link" aria-label="Facebook">
-//                 <FaFacebookF className="socialmedia-icon facebook" />
-//               </a>
-//               <a href="#" className="footer_social-link" aria-label="Twitter">
-//                 <FaTwitter className="socialmedia-icon twitter" />
-//               </a>
-//               <a href="#" className="footer_social-link" aria-label="Instagram">
-//                 <FaInstagram className="socialmedia-icon instagram" />
-//               </a>
-//               <h3>© Copyright 2025</h3>
-//             </section>
-//       </div>
-//     </footer>
-//   );
-// }
