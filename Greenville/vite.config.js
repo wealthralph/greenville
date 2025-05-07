e48304cec1de24e70a60502179fa906ba871eb33
catalyst <wealthralph@gmail.com>
2025-05-07 13:12:34 +0100

    update greenvile paths

--- Greenville/vite.config.js
+++ Greenville/vite.config.js
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 600 * 1024, // Set to 600KB, for example
+  },
+});
